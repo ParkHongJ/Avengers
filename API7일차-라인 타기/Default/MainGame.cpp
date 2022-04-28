cd48e4590f2010c698f6ae61ffe1e7c8a4b84a3c
DESKTOP-E55IUE1\JJUNI <qkrghdwns98@naver.com>
2022-04-28 16:17:17 +0900

    더블버퍼링 추가

--- API7일차-라인 타기/Default/MainGame.cpp
+++ API7일차-라인 타기/Default/MainGame.cpp
@@ -58,7 +58,23 @@ void CMainGame::Late_Update(void)
 }
 
 void CMainGame::Render(void)
-{
+{ 
+	//더블버퍼링 출처 : https://blog.naver.com/PostView.nhn?isHttpsRedirect=true&blogId=kyed203&logNo=20187732037&beginTime=0&jumpingVid=&from=section&redirect=Log&widgetTypeCall=true
+	/** 더블버퍼링 시작처리입니다. **/
+	HDC MemDC, tmpDC;
+	HBITMAP BackBit, oldBackBit;
+	RECT bufferRT;
+
+	GetClientRect(g_hWnd, &bufferRT);
+	MemDC = CreateCompatibleDC(m_hDC);
+	BackBit = CreateCompatibleBitmap(m_hDC, bufferRT.right, bufferRT.bottom);
+	oldBackBit = (HBITMAP)SelectObject(MemDC, BackBit);
+	PatBlt(MemDC, 0, 0, bufferRT.right, bufferRT.bottom, WHITENESS);
+	tmpDC = m_hDC;
+	m_hDC = MemDC;
+	MemDC = tmpDC;
+
+	// TODO: 여기에 그리기 코드를 추가합니다.
 	Rectangle(m_hDC, 0, 0, WINCX, WINCY);
 
 	CLineMgr::Get_Instance()->Render(m_hDC);
@@ -74,6 +90,18 @@ void CMainGame::Render(void)
 		m_iFPS = 0;
 		m_dwTime = GetTickCount();
 	}
+
+	/** 더블버퍼링 끝처리 입니다. **/
+	tmpDC = m_hDC;
+	m_hDC = MemDC;
+	MemDC = tmpDC;
+	GetClientRect(g_hWnd, &bufferRT);
+	BitBlt(m_hDC, 0, 0, bufferRT.right, bufferRT.bottom, MemDC, 0, 0, SRCCOPY);
+	SelectObject(MemDC, oldBackBit);
+	DeleteObject(BackBit);
+	DeleteDC(MemDC);
+	
+	
 
 }
 
